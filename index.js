a828d599c7d2b06c2f524a9e54a0470d3729f1d6
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2019-04-16 17:57:04 -0700

    testing catch clause

--- index.js
+++ index.js
@@ -15,10 +15,21 @@ app.use(bodyParser());
 // add some security-related headers to the response
 app.use(helmet())
 connectorMongodb.then(() => {
+    console.log('Connected to MongoDB successfully')
     app.get('*', (req, res) => {
         res.set('Content-Type', 'text/html')
         res.send(200, `
             <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
+            <h2>Go to <a href="/about">/about</a></h2>
+        `)
+        mongoose.connection.close()
+    })
+})
+.catch(err => {
+    app.get('*', (req, res) => {
+        res.set('Content-Type', 'text/html')
+        res.send(200, `
+            <h1><marquee direction=right>Failed connection ${err.message}</marquee></h1>
             <h2>Go to <a href="/about">/about</a></h2>
         `)
         mongoose.connection.close()
